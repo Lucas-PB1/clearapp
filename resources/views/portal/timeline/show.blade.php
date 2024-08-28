883fb82bee27ff3a46591fc2768438a06c2fb113
Lucas-PB1 <lucasoaresnet@gmail.com>
2024-08-28 14:31:50 -0300

    Correção do portal

--- resources/views/portal/timeline/show.blade.php
+++
@@ -1,7 +0,0 @@
-<x-timeline-layout>
-    <h1>{{ $data->{'nome-do-evento'} }}</h1>
-    <div class="container text-center">
-        <img src="{{ url($data->destaque->path) }}" alt="destaque-{{ $data->id }}" width="50%">
-        <div>@php echo($data->texto) @endphp</div>
-    </div>
-</x-timeline-layout>
